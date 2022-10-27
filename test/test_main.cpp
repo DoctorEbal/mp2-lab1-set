f12fb5cd6748cc5ef068c1cd6892bf76b32d0947
DoctorEbal <olegan7966@list.ru>
2022-10-27 17:03:30 +0300

    tbitfield done

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,11 @@
 #include <gtest.h>
 
+//#include <tbitfield.h>
+//#include < iostream>
+using namespace std;
 int main(int argc, char **argv) {
+    //cout << sizeof(TELEM);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+   // cout << sizeof(TELEM);
 }
